9bd5a3091ef5caf9806a6a9f3391dd1d2e8bae51
Dinh Dat <tddat19102004@gmail.com>
2026-05-05 21:58:30 +0700

    fix CORS

--- BE/src/main/java/com/exam/attendance/config/SecurityConfig.java
+++ BE/src/main/java/com/exam/attendance/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .securityMatcher("/api/**")
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(List.of("http://localhost:5173","https://exam-attendance-d917b.web.app"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
